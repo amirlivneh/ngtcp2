3ba296bc8d21bb1a6429ea6e5028f3bedd906408
Amir Livneh <alivneh@fb.com>
2024-12-27 07:07:56 -0500

    Increase max_len

--- .clusterfuzzlite/build.sh
+++ .clusterfuzzlite/build.sh
@@ -24,6 +24,8 @@ $CXX $CXXFLAGS -std=c++17 -Ilib/includes -Ilib -I. -DHAVE_CONFIG_H \
      fuzz/read_write_handshake_pkt.cc -o $OUT/read_write_handshake_pkt \
      $LIB_FUZZING_ENGINE lib/.libs/libngtcp2.a
 
+cp $SRC/*.options $OUT/
+
 zip -j $OUT/decode_frame_seed_corpus.zip fuzz/corpus/decode_frame/*
 zip -j $OUT/ksl_seed_corpus.zip fuzz/corpus/ksl/*
 zip -j $OUT/read_write_pkt.zip fuzz/corpus/read_write_pkt/*
