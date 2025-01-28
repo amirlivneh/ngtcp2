8985d2d7789142b611f6431eccc66560931e9943
Amir Livneh <alivneh@fb.com>
2025-01-28 14:05:58 -0500

    fuzz: Add dictionary

--- .clusterfuzzlite/build.sh
+++ .clusterfuzzlite/build.sh
@@ -28,3 +28,6 @@ zip -j $OUT/decode_frame_seed_corpus.zip fuzz/corpus/decode_frame/*
 zip -j $OUT/ksl_seed_corpus.zip fuzz/corpus/ksl/*
 zip -j $OUT/read_write_pkt.zip fuzz/corpus/read_write_pkt/*
 zip -j $OUT/read_write_handshake_pkt.zip fuzz/corpus/read_write_handshake_pkt/*
+
+# cp .clusterfuzzlite/*.options $OUT/
+cp fuzz/*.dict $OUT/
